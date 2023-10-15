01b4ad1ecd9ed735c9dfaa11862efc434803b5db
Mger-arzi <mger54598@gmail.com>
2023-10-15 08:57:19 +0300

    add sprite

---
+++ src/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Header = () => {
+	return (
+		<StyledHeader>
+			
+		</StyledHeader>
+	);
+};
+const StyledHeader = styled.div `
+	
+`
---
+++ src/icon/icon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const icon = () => {
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
